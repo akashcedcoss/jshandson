9bbe1041e1de2eedb2d31a69e4a95de43db6a4bd
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 14:21:43 +0530

    Manipulating Complex Objects

--- src/index.js
+++ src/index.js
@@ -703,3 +703,20 @@ const myDog = {
   
     // Only change code above this line
   }  
+  const myMusic = [{
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": ["CD","8T","LP"],
+    "gold": true
+  },
+  {
+    "artist": "Bob",
+    "title": "Hello",
+    "release_year": 1950,
+    "formats": [
+      "CS",
+      "8T"
+    ]
+  }
+];
