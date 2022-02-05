d81037c6bc0f706f935d51cd4128e5edb779734c
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 14:06:15 +0530

    Delete Properties from a Javascript Object

--- src/index.js
+++ src/index.js
@@ -657,5 +657,15 @@ const myDog = {
   };
   myDog.bark = "woof";
   
-
+  // Setup
+const myDog = {
+    "name": "Happy Coder",
+    "legs": 4,
+    "tails": 1,
+    "friends": ["freeCodeCamp Campers"],
+    "bark": "woof"
+  };
+  
+  // Only change code below this line
+  delete myDog.tails;
   
