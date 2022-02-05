707cece4781aadef7d1294ae829c05e399f10bde
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 11:42:18 +0530

    Constructing Strings with variables

--- src/index.js
+++ src/index.js
@@ -79,3 +79,5 @@ const myStr = "FirstLine\n\t\\SecondLine\nThirdLine";
 const myStr = "This is the start." + " " + "This is the end.";
 let myStr = "This is the first sentence. ";
 myStr += "This is the second sentence.";
+const myName = "Akash";
+const myStr = "My name is " + myName + "and I am well!";
