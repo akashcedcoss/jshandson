0f2aa4fab2ccb3290ea5537b608bb6c9a625f995
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 14:45:31 +0530

    Iterate with JavaScript For loops

--- src/index.js
+++ src/index.js
@@ -796,3 +796,15 @@ while (i >=0) {
   myArray.push(i);
   i--;
 } 
+var ourArray = [];
+
+for (var i = 0; i < 5; i++) {
+  ourArray.push(i);
+}
+
+// Setup
+var myArray = [];
+
+for (var i = 1; i < 6; i++) {
+  myArray.push(i);
+}
