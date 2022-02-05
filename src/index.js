cba2fce9d73a988690d853f96c61b12657b3ee66
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 13:43:22 +0530

    Returning Boolean Values from Functions

--- src/index.js
+++ src/index.js
@@ -537,3 +537,10 @@ function nextInLine(arr, item) {
   }
   
   chainToSwitch(7);
+  function isLess(a, b) {
+    // Only change code below this line
+    return a<b;
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
