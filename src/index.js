93136c781e65bde0217cd2182eddb228460efe31
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 14:13:09 +0530

    Testing Objects for Properties

--- src/index.js
+++ src/index.js
@@ -688,4 +688,18 @@ const myDog = {
   }
   
   // Change this value to test
-  phoneticLookup("charlie");  
+  phoneticLookup("charlie");
+  function checkObj(obj, checkProp) {
+    // Only change code below this line
+  
+    var myObj = {
+      gift: "pony",
+      pet: "kitten",
+      bed: "sleigh",
+      city: "Seattle"
+    };
+  
+    return (obj.hasOwnProperty(checkProp)) ? myObj[checkProp] : "Not Found";
+  
+    // Only change code above this line
+  }  
