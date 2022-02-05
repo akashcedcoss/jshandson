3a1f4bd3da769f3f4d57dfcf133438f8ef56d931
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 12:57:33 +0530

    Practice comparing different values

--- src/index.js
+++ src/index.js
@@ -260,3 +260,11 @@ function nextInLine(arr, item) {
   }
   
   testStrict(10);
+  function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
