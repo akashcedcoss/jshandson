7691b8cc162ceaf0821be08c25a63d39df38ee66
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 12:17:40 +0530

    Manipulate Arrays With Shift

--- src/index.js
+++ src/index.js
@@ -128,3 +128,7 @@ const myArray = [["John", 23], ["cat", 2]];
 
 // Only change code below this line
 const removedFromMyArray = myArray.pop
+const myArray = [["John", 23], ["dog", 3]];
+
+// Only change code below this line
+const removedFromMyArray = myArray.shift();
