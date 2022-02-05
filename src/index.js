5cde417dacdb80b97c08a2289497fe149d820901
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 13:33:40 +0530

    Adding a default option in Switch Statements

--- src/index.js
+++ src/index.js
@@ -458,3 +458,27 @@ function nextInLine(arr, item) {
   
   // Change this value to test
   caseInSwitch(1);
+  function switchOfStuff(val) {
+    let answer = "";
+    // Only change code below this line
+    switch(val){
+    case "a":
+    answer = "apple";
+    break;
+    case "b":
+    answer = "bird";
+    break;
+    case "c":
+    answer = "cat";
+    break;
+    default:
+    answer = "stuff";
+    break;
+    }
+  
+  
+    // Only change code above this line
+    return answer;
+  }
+  
+  switchOfStuff(1);
