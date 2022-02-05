bdba4d8b2881fa6b41c55bb53f65fc2ec60f90c6
akashcedcoss <akashkumar2022jan@cedcoss.com>
2022-02-05 13:05:27 +0530

    Comparison with the Less Than Operator

--- src/index.js
+++ src/index.js
@@ -297,4 +297,17 @@ function nextInLine(arr, item) {
   }
   
   testGreaterThan(10);
-  v
+  function testLessThan(val) {
+    if (val < 25) {  // Change this line
+      return "Under 25";
+    }
+  
+    if (val < 55) {  // Change this line
+      return "Under 55";
+    }
+  
+    return "55 or Over";
+  }
+  
+  testLessThan(10);
+  
